e3ddc9446264502abcae6591a8b59663bc71fe6f
zhadyrra <zhadyrra@gmail.com>
2024-06-14 23:02:23 +0500

    first commit diploma_1_part

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
 
 public class BurgerTest {
     private Burger burger;
@@ -14,13 +16,19 @@ public class BurgerTest {
     private Ingredient ingredient2;
     @Before
     public void setUp() {
+        String name = "BunBun";
         burger = new Burger();
         bun = mock(Bun.class);
+        when(bun.getName()).thenReturn(name);
         when(bun.getPrice()).thenReturn(100.0F);
         ingredient1 = mock(Ingredient.class);
         when(ingredient1.getPrice()).thenReturn(1f);
+        when(ingredient1.getType()).thenReturn(FILLING);
+        when(ingredient1.getName()).thenReturn("Beef");
         ingredient2 = mock(Ingredient.class);
         when(ingredient2.getPrice()).thenReturn(0.3f);
+        when(ingredient2.getType()).thenReturn(SAUCE);
+        when(ingredient2.getName()).thenReturn("Ketchup");
     }
     @Test
     public void testSetBuns() {
@@ -53,14 +61,16 @@ public class BurgerTest {
         assertEquals(ingredient2, burger.ingredients.get(0));
     }
 
-  /*  @Test
+    @Test
     public void testGetReceipt() {
         burger.setBuns(bun);
         burger.addIngredient(ingredient1);
         burger.addIngredient(ingredient2);
-
-        String expectedReceipt = String.format("(==== %s ====)%n= Cheese =%n= Lettuce =%n(==== %s ====)%n%nPrice: %f%n",
-                "Sesame", "Sesame", burger.getPrice());
+        String actual = burger.getReceipt();
+        System.out.println(actual);
+        String expectedReceipt = String.format(
+        "(==== BunBun ====)%n= filling Beef =%n= sauce Ketchup =%n(==== BunBun ====)%n%nPrice: 201,300003%n");
         assertEquals(expectedReceipt, burger.getReceipt());
-    }*/
-}
+    }
+
+  }
